9f40c036516807e5866488fdafaf2082027f6f5d
0xdefiant <anthony@defisteward.com>
2023-10-03 16:03:57 -0700

    single endpoint

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 import openai
 from flask import Flask, request, jsonify, render_template
 from dotenv import load_dotenv
+from flask_cors import CORS
 import requests
 
 load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
+CORS(app)
 
 # Retrieve the API key from environment variables
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -15,46 +17,45 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 def home():
     return render_template('home/index.html')
 
-@app.route('/get_token_data', methods=['GET'])
-def get_token_data():
-    network = request.form['network']
-    token_address = request.form['tokens']
-    url = f'https://api.geckoterminal.com/api/v2/networks/{network}/tokens/{token_address}?include=include'
-    response = requests.get(url, headers={'accept': 'application/json'})
-    print(f"Gecko API Response Code: {response.status_code}")  
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        print(f"Error Message: {response.content}") 
-        return jsonify({'error': 'Unable to fetch data'}), response.status_code
-
-@app.route('/get-trade', methods=['POST'])
-def get_trade():
+@app.route('/get-portfolio-suggestion', methods=['POST'])
+def get_portfolio_suggestion():
     data = request.json
-    holdings = data.get('holdings', [])
+    network = data['network']
+    tokens = data['tokens']
+    
+    # Fetch data from Geckoterminal
+    url = f'https://api.geckoterminal.com/api/v2/networks/{network}/tokens/{tokens}?include=include'
+    gecko_response = requests.get(url, headers={'accept': 'application/json'})
 
-    # Convert list of cryptocurrencies into a string for the prompt
-    holdings_str = ", ".join(holdings)
+    if gecko_response.status_code != 200:
+        return jsonify({"error": f"Failed to fetch data from Geckoterminal: {gecko_response.content}"}), 500
+    
+    token_data = gecko_response.json()
 
+    # Extract relevant info from Geckoterminal's response
+    token_name = token_data['data']['attributes']['name']
+    token_price_usd = token_data['data']['attributes']['price_usd']
+    token_symbol = token_data['data']['attributes']['symbol']
+    token_volume_24h = token_data['data']['attributes']['volume_usd']['h24']
+    total_supply = token_data['data']['attributes']['total_supply']
+
+    # Create the message for the OpenAI model
     message = {
         "role": "system",
         "content": "You are a crypto portfolio allocation specialist, that builds portfolio models from information that you are given."
     }
     user_message = {
         "role": "user",
-        "content": f"I have the following cryptocurrencies: {holdings_str}. Please create a model portfolio."
+        "content": f"I'm considering {token_name} ({token_symbol}). It's currently priced at ${token_price_usd} with a 24h trading volume of ${token_volume_24h}, and supply of {total_supply}. Given this, how should I structure my portfolio?"
     }
 
-    response = openai.ChatCompletion.create(
+    openai_response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[message, user_message]
     )
 
-    # Get the AI's response
-    ai_response = response.choices[0].message['content']
-
+    ai_response = openai_response.choices[0].message['content']
     return jsonify({"portfolio": ai_response})
-
 
 if __name__ == '__main__':
     app.run(port=5000)
